d409e0d28c4b378660c23d9f6bb40b3e2a7df76b
jpukabeals <pukab001@umn.edu>
2022-06-17 17:26:48 -0500

    added some more updates

--- JPB_r-script-fix_17Jun2022.R
+++ JPB_r-script-fix_17Jun2022.R
@@ -5,8 +5,8 @@
 # This code provides outputs of p-values among all treatment comparisons using
 # the drcte package
 
-# for dplyr::filter
-library(dplyr)
+# for data manipulation at the end
+library(tidyverse)
 
 # prevent scientific notation so p-values are clearer
 options(scipen = 999)
@@ -55,3 +55,35 @@ for(i in 1:15){
                          pvalu)
   print(pval_list)
 }
+
+sink("output_17Jun2022.txt")
+print(pval_list)
+sink()
+
+
+for(i in 1:15){
+  subs <- pairs[i,]
+  dat.sub <- filter(dat, trt==subs[1] | trt== subs[2])
+  mod <- drmte(nSeeds ~ timeBef + timeAf, fct = LL.3(),
+               curveid = trt, data = dat.sub)
+  pvalu <- compCDF(mod, units = dat.sub$rep)$pval
+  trt.comparison <- paste(
+    # "comaring treatments",
+                          subs[1],
+                          "--",
+                          subs[2])
+  pval_list[[i]] <- list(trt.comparison,
+                         pvalu)
+  print(pval_list)
+}
+
+do.call(rbind.data.frame, pval_list) -> ugly.combined
+
+ugly.combined %>% 
+  rename(trt.comparison = colnames(ugly.combined)[1],
+         p.value = colnames(ugly.combined)[2]) %>% 
+  mutate(p.value = round(p.value,digits = 5)) %>% 
+  write.csv("17Jun2022-model-outputs.csv",
+            row.names = F)
+
+
